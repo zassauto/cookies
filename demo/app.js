d0aa96f77692973a458cc725b34b455046e4243a
Younes <khalfii.youunes@gmail.com>
2022-01-21 23:10:05 +0100

    Update app.js

--- demo/app.js
+++ demo/app.js
@@ -76,7 +76,7 @@ cc.run({
         'en': {
             consent_modal: {
                 title: cookie + 'Le respect de votre vie privée est notre priorité',
-                description: 'Notre site internet et les sociétés partenaires utilisent des cookies. Ces cookies nous permettent de personnaliser votre expérience utilisateur, mesurer l’audience, collecter des statistiques de visite pour l’optimisation de la publicité et l’amélioration des contenus. Vous pouvez gérer vos préférences des cookies, accepter ou refuser le dépôt de l’ensemble des cookies. Un lien &quot;Gérer mes cookies&quot; sur notre site vous permet également de modifier vos choix à tout moment. <button type="button" data-cc="c-settings">Show cookie settings</button><br><button type="button" data-cc="accept-all">Accept all cookies</button><br><button type="button" data-cc="accept-necessary">Reject All</button',
+                description: 'Notre site internet et les sociétés partenaires utilisent des cookies. Ces cookies nous permettent de personnaliser votre expérience utilisateur, mesurer l’audience, collecter des statistiques de visite pour l’optimisation de la publicité et l’amélioration des contenus. Vous pouvez gérer vos préférences des cookies, accepter ou refuser le dépôt de l’ensemble des cookies. Un lien &quot;Gérer mes cookies&quot; sur notre site vous permet également de modifier vos choix à tout moment. <button type="button" data-cc="c-settings">Gérer mes préférences</button><br><button type="button" data-cc="accept-all">Tout accepter</button><br><button type="button" data-cc="accept-necessary">Tout refuser</button',
                 primary_btn: {
                     text: 'Accept all',
                     role: 'accept_all'              // 'accept_selected' or 'accept_all'
